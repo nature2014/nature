66cdff13aded608a1f28fc069c5a395932fe46af
limin.llm <116352437@qq.com>
2014-10-19 21:36:07 +0800

    打造一个基本的Policy引擎！

--- src/policy/AbstractPolicy.java
+++ src/policy/AbstractPolicy.java
@@ -1,19 +1,81 @@
 package policy;
 
+import com.greenpineyu.fel.FelEngine;
+import com.greenpineyu.fel.FelEngineImpl;
+import com.greenpineyu.fel.context.FelContext;
 import com.greenpineyu.fel.function.CommonFunction;
 import com.greenpineyu.fel.function.Function;
+import org.apache.commons.lang.BooleanUtils;
 import org.apache.commons.lang.math.NumberUtils;
+import policy.schema.ConditionEntry;
+import policy.schema.Parameter;
 
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by peter on 2014/10/16.
  * 实现一些基本的方法或者扩展方法,同时实现扩展的函数，采用标准的方法：Policy.xml中的定义
  */
 public abstract class AbstractPolicy implements Policy {
-    public boolean checkCondition(PolicyContext policyContext) {
+    protected PolicyXmlManager policyXmlManager = PolicyXmlManager.getInstance();
 
-        return false;
+    public boolean checkCondition(PolicyContext policyContext) {
+        List<PolicyCondition> policyConditions = policyContext.getPolicyConditions();
+        //迭代每个Condition
+        boolean globalBool = true;
+        for (int i = 0; i < policyConditions.size(); i++) {
+            PolicyCondition policyCondition = policyConditions.get(i);
+            //查询Policy.xml模版定义的条件
+            ConditionEntry conditionEntry = policyXmlManager.queryConditionById(policyCondition.getId());
+
+            //参数化，用交互的参数覆盖缺省的
+            List<Parameter> parameters = conditionEntry.getParameter();
+            boolean checkCondition = true;
+            FelEngine fel = new FelEngineImpl();
+            FelContext context = fel.getContext();
+
+            for (int j = 0; j < parameters.size(); j++) {
+                Parameter parameter = parameters.get(i);
+                String name = parameter.getName();
+                Map<String, Object> activeParameter = policyCondition.getParameters();
+                Object value = activeParameter.get(name) != null ? activeParameter.get(name) != null : parameter.getDefault();
+                //数据类型转化
+                if (value != null) {
+                    String dateType = parameter.getDataType();
+                    if (dateType.equals("string")) {
+                        value = value.toString();
+                    } else if (dateType.equals("int")) {
+                        value = NumberUtils.toInt(value.toString());
+                    } else if (dateType.equals("long")) {
+                        value = NumberUtils.toLong(value.toString());
+                    } else if (dateType.equals("float")) {
+                        value = NumberUtils.toFloat(value.toString());
+                    } else if (dateType.equals("boolean")) {
+                        value = BooleanUtils.toBoolean(value.toString());
+                    }
+                } else {
+                    //数据错误，也直接是false
+                    checkCondition = false;
+                    break;
+                }
+                context.set(name, value);
+            }
+
+            //表示上面无解析错误
+            if (checkCondition) {
+                //利用FEL评估最后的表达式结果
+                Boolean returnBoolean = (Boolean) fel.eval(conditionEntry.getExpression(), context);
+                globalBool = globalBool && returnBoolean;
+                if (!globalBool) {
+                    //有一个false,就直接跳出
+                    break;
+                }
+            }
+        }
+        return globalBool;
     }
 
     public static Function caculateDayOfWeek() {
--- src/policy/PolicyEngine.java
+++ src/policy/PolicyEngine.java
@@ -7,18 +7,78 @@ import com.greenpineyu.fel.context.FelContext;
 import com.greenpineyu.fel.function.CommonFunction;
 import com.greenpineyu.fel.function.Function;
 import org.apache.commons.lang.math.NumberUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import policy.schema.ActionEntry;
 
 import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * Created by peter on 2014/10/16.
  */
 public abstract class PolicyEngine {
+    private PolicyXmlManager policyXmlManager = PolicyXmlManager.getInstance();
+    private final static Logger LOG = LoggerFactory.getLogger(PolicyXmlManager.class);
+    protected final static ConcurrentHashMap<String, Policy> cachedPolicy = new ConcurrentHashMap<String, Policy>();
 
-    public List<PolicyResult> sendToEngine(PolicyContext policyContext) {
-        List<PolicyResult> policyResults = Arrays.<PolicyResult>asList();
-        EventIf eventIf = policyContext.getEvent();
-        return policyResults;
+    /**
+     * 给外部调用此消息接口,依赖于正在开发的领域对象关系
+     * Task关联多个Policies , 一个Policy映射多个Condition和1个Action
+     *
+     * @param eventif
+     * @return
+     */
+    public List<PolicyResult> processPolicies(EventIf eventif) {
+
+        return null;
+    }
+
+    /**
+     * 构建者模式
+     *
+     * @param policyCondition
+     * @param policyAction
+     * @return
+     */
+    public PolicyContext builderPolicy(List<PolicyCondition> policyCondition, PolicyAction policyAction) {
+        /**
+         *  在炉子里锻造PolicyContext，基本上从eventIf可以推断出必要的PolicyCondition和PolicyAction
+         *
+         *  需要根据taskid去查找对应的关联关系表
+         */
+        return new PolicyContext();
+    }
+
+    private PolicyResult processPolicy(PolicyContext policyContext) {
+        PolicyAction policyAction = policyContext.getPolicyActions();
+        String actionId = policyAction.getId();
+        ActionEntry actionEntry = policyXmlManager.queryActionById(actionId);
+        if (actionEntry != null) {
+            String classPath = actionEntry.getClassPath();
+            try {
+                Policy policy = null;
+                //读取缓存对象
+                if (cachedPolicy.contains(actionId)) {
+                    policy = cachedPolicy.get(actionId);
+                } else {
+                    policy = (Policy) Class.forName(classPath).newInstance();
+                    Policy oldPolicy = cachedPolicy.putIfAbsent(actionId, policy);
+                    if (oldPolicy != null) {
+                        //还是用旧的对象，保持缓存一致
+                        policy = oldPolicy;
+                    }
+                }
+
+                if (policy.checkCondition(policyContext)) {
+                    PolicyResult policyResult = policy.execute(policyContext);
+                    return policyResult;
+                }
+            } catch (Exception en) {
+                LOG.error("锻造对象失败{}", en.getMessage());
+            }
+        }
+        return null;
     }
 
     public static void main(String[] args) {
